175f5832f7c0c288a4b88ac992dac241c395e562
ravencler@gmail.com <ravencler@gmail.com>
2025-12-04 18:38:41 +0700

    fix

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -95,45 +95,45 @@
 				<div class="order-block">
 					<div class="grid">
 						<div class="left-block">
-{{--							<div class="design-price">--}}
-{{--								<div class="title">--}}
-{{--									Требуется дизайн?--}}
-{{--									<div class="icon">--}}
-{{--										<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">--}}
-{{--											<path d="M9.14648 9.07361C9.31728 8.54732 9.63015 8.07896 10.0508 7.71948C10.4714 7.36001 10.9838 7.12378 11.5303 7.03708C12.0768 6.95038 12.6362 7.0164 13.1475 7.22803C13.6587 7.43966 14.1014 7.78875 14.4268 8.23633C14.7521 8.68391 14.9469 9.21256 14.9904 9.76416C15.0339 10.3158 14.9238 10.8688 14.6727 11.3618C14.4215 11.8548 14.0394 12.2685 13.5676 12.5576C13.0958 12.8467 12.5533 12.9998 12 12.9998V14.0002M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21ZM12.0498 17V17.1L11.9502 17.1002V17H12.0498Z" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>--}}
-{{--										</svg>--}}
-{{--									</div>--}}
-{{--								</div>--}}
-{{--								<div class="list">--}}
-{{--									<label class="radio">--}}
-{{--										<input type="radio" name="design" value="own" checked="checked">--}}
-{{--										<span class="check"></span>--}}
-{{--										<span class="label">У меня есть готовый дизайн</span>--}}
-{{--									</label>--}}
-{{--									<label class="radio">--}}
-{{--										<input type="radio" name="design" value="order">--}}
-{{--										<span class="check"></span>--}}
-{{--										<span class="label">Заказать услуги дизайнера</span>--}}
-{{--									</label>--}}
-{{--								</div>--}}
-{{--								<label class="file">--}}
-{{--									<input type="file" name="file" id="designFile">--}}
-{{--									<span class="text" id="fileText">Загрузить дизайн-макет или файл</span>--}}
-{{--									<span class="btn">--}}
-{{--										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">--}}
-{{--											<path d="M4 8H8M8 8H12M8 8V12M8 8V4" stroke="#F6F6F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>--}}
-{{--										</svg>--}}
-{{--										Выбрать файл--}}
-{{--									</span>--}}
-{{--								</label>--}}
-{{--								<div class="all-price">--}}
-{{--									<div class="caption">Итого</div>--}}
-{{--									<div class="val" id="totalPrice">{{ number_format($price_good, 0, ',', ' ') }} ₽</div>--}}
-{{--								</div>--}}
-{{--							</div>--}}
+							<div class="design-price">
+								<div class="title">
+									Требуется дизайн?
+									<div class="icon">
+										<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M9.14648 9.07361C9.31728 8.54732 9.63015 8.07896 10.0508 7.71948C10.4714 7.36001 10.9838 7.12378 11.5303 7.03708C12.0768 6.95038 12.6362 7.0164 13.1475 7.22803C13.6587 7.43966 14.1014 7.78875 14.4268 8.23633C14.7521 8.68391 14.9469 9.21256 14.9904 9.76416C15.0339 10.3158 14.9238 10.8688 14.6727 11.3618C14.4215 11.8548 14.0394 12.2685 13.5676 12.5576C13.0958 12.8467 12.5533 12.9998 12 12.9998V14.0002M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21ZM12.0498 17V17.1L11.9502 17.1002V17H12.0498Z" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+										</svg>
+									</div>
+								</div>
+								<div class="list">
+									<label class="radio">
+										<input type="radio" name="design" value="own" checked="checked">
+										<span class="check"></span>
+										<span class="label">У меня есть готовый дизайн</span>
+									</label>
+									<label class="radio">
+										<input type="radio" name="design" value="order">
+										<span class="check"></span>
+										<span class="label">Заказать услуги дизайнера</span>
+									</label>
+								</div>
+								<label class="file">
+									<input type="file" name="file" id="designFile">
+									<span class="text" id="fileText">Загрузить дизайн-макет или файл</span>
+									<span class="btn">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M4 8H8M8 8H12M8 8V12M8 8V4" stroke="#F6F6F6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+										</svg>
+										Выбрать файл
+									</span>
+								</label>
+								<div class="all-price">
+									<div class="caption">Итого</div>
+									<div class="val" id="totalPrice">{{ number_format($price_good, 0, ',', ' ') }} ₽</div>
+								</div>
+							</div>
                             <style>
-                                .order-form .order-block .left-block {
-                                    border: none;
+                                .file {
+                                    display: none;
                                 }
                             </style>
 						</div>
