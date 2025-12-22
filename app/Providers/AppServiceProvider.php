0969996d83c4b3b78c8dfbd0eb8c2e902dda026b
ravencler@gmail.com <ravencler@gmail.com>
2025-12-23 00:40:56 +0700

    Create AssetHelper to support dynamic asset paths in @assets directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,27 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Blade::directive('assets', function ($entry) {
-            $manifestPath = public_path('build/manifest.json');
-
-            if (!file_exists($manifestPath)) {
-                return "<!-- manifest.json not found -->";
-            }
-
-            $manifest = json_decode(file_get_contents($manifestPath), true);
-            $entry = trim($entry, "\"'");
-
-            if (!isset($manifest[$entry])) {
-                return "<!-- Asset $entry not found in manifest -->";
-            }
-
-            $file = $manifest[$entry];
-            $src = '/build/' . $file['file'];
-
-            if (str_ends_with($entry, '.css')) {
-                return "<link rel=\"stylesheet\" href=\"{$src}\" />";
-            }
-
-            return "<script type=\"module\" src=\"{$src}\"></script>";
+            return "<?php echo \\App\\Helpers\\AssetHelper::render({$entry}); ?>";
         });
     }
 }
