b53a969908cdabbf7c465dfa43d5c97514f6b555
ravencler@gmail.com <ravencler@gmail.com>
2025-12-23 00:29:40 +0700

    Add explicit Vite configuration

---
+++ config/vite.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'input' => [
+        'resources/css/app.css',
+        'resources/css/product.css',
+        'resources/js/pages/home.js',
+        'resources/js/pages/product-146.js',
+        'resources/js/pages/product-155.js',
+        'resources/js/pages/product-156.js',
+        'resources/js/pages/product-157.js',
+        'resources/js/pages/product-158.js',
+        'resources/js/pages/product-151.js',
+        'resources/js/pages/product-154.js',
+        'resources/js/pages/product-159.js',
+        'resources/js/pages/product-160.js',
+        'resources/js/pages/product-161.js',
+    ],
+    'ssr' => null,
+];
